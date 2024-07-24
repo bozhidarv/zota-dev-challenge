bb5a0086a0665064f472f0402dca898d0e84eeb1
Bozhidar Videnov <bozh.bv03@gmail.com>
2024-07-24 13:06:52 +0300

    feat: add .gitignore and server

--- cmd/zota-dev-challenge/main.go
+++ cmd/zota-dev-challenge/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	println("Hello, World!")
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello, World!"))
+		if err != nil {
+			http.Error(w, "Error writing response", http.StatusInternalServerError)
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+	http.Handle("/", r)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
